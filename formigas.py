0fa61884496ffb97407cdf2fa260cea8d29aa263
paulacampigotto <paula_campigotto@hotmail.com>
2019-09-02 23:43:05 -0300

    update

--- formigas.py
+++ formigas.py
@@ -1,37 +1,69 @@
-#from Tkinter import *
+import pygame
+from pygame.locals import *
 from pprint import pprint
 from random import choices
 from random import randint
 from math import ceil
+import threading
 
 
-size = 20
+
+
+size = 50
 radius = 1
-deadAnts = ceil(size*size * .4)
+kPick = 0.01
+kDrop = 0.2
+
+deadAnts = ceil(size*size * .1)
 aliveAnts = ceil(size*size * .05)
 aliveAntsList = []
 cells1 = [[0 for i in range(size)] for i in range(size)]
-cells2 = [[0 for i in range(size)] for i in range(size)]
+
+pygame.init()
+win = pygame.display.set_mode((size*10,size*10))
+pygame.display.set_caption("First Game")
+
+def screen():
+    while True:
+        pygame.time.delay(100)
 
 def display():
-    dispose(cells1,deadAnts)
-    dispose(cells2,aliveAnts)
-    pprint(cells1)
-    for i in range(10000):
+    dispose(deadAnts,False)
+    dispose(aliveAnts,True)
+    for i in range(size):
+        for j in range(size):
+            if(cells1[i][j] == 0):
+                print(" ", end="")
+            else: print("X", end="")
+        print()
+    print("-"*size)
+    #pprint(cells1)
+    for i in range(100000):
         for j in aliveAntsList:
             j.live()
-        #    if(aliveAntsList.index(j)==0): print(j.position)
+            #print(str(aliveAntsList.index(j)) + ':'+ str(j.position), end=" "
+        #print("\n")
 
 
-def prob(x,y):
-    global size, radius
+def probPick(x,y):
+    global size, radius, kPick
     itemsAround = 0
     for i in range(-1*radius,2*radius):
-        for j in range(-1*radius,2*radius):
+        for j in range(-1*radius,1*radius):
             if (i==0 and j==0) or x+i<0 or y+j<0 or x+i>=size or y+j>=size:
                 continue
             if cells1[x+i][y+j] == 1: itemsAround+=1
-    return (itemsAround+1)/10 # cuidado
+    return (kPick/(kPick + itemsAround)) ** 2 # Função P do artigo
+
+def probDrop(x,y):
+    global size, radius, kDrop
+    itemsAround = 0
+    for i in range(-1*radius,2*radius):
+        for j in range(-1*radius,10*radius):
+            if (i==0 and j==0) or x+i<0 or y+j<0 or x+i>=size or y+j>=size:
+                continue
+            if cells1[x+i][y+j] == 1: itemsAround+=1
+    return (itemsAround/(kDrop+itemsAround)) ** 2 # Função P do artigo
 
 def emptyCell(position):
     return cells1[position[0]][position[1]] == 0
@@ -40,21 +72,25 @@ class AliveAnt:
     def __init__(self, position):
         self.hasItem = False
         self.position = position
-        self.memory = ()
 
     def live(self):
+        global cells1
         self.move()
-        probability = prob(self.position[0], self.position[1])
         if emptyCell(self.position) and self.hasItem:
+            probability = probDrop(self.position[0], self.position[1])
             choice = choices([1,0], [probability, 1-probability])
             if choice == [1]: #drop
                 cells1[self.position[0]][self.position[1]] = 1
+                pygame.draw.circle(win, (255,0,0), (self.position[0]*10, self.position[1]*10), 2)
+                pygame.display.update()
                 self.hasItem = False
-                self.memory = self.position
         elif not emptyCell(self.position) and not self.hasItem:
+            probability = probPick(self.position[0], self.position[1])
             choice = choices([1,0], [1-probability, probability])
             if (choice == [1]): #pick
                 cells1[self.position[0]][self.position[1]] = 0
+                pygame.draw.circle(win, (0,0,0), (self.position[0]*10, self.position[1]*10), 2)
+                pygame.display.update()
                 self.hasItem = True
 
     def move1(self, direction):
@@ -79,38 +115,38 @@ class AliveAnt:
         global size
         if x>=0 and x<size and y>=0 and y<size:
             self.position = (x,y)
-            cells2[x][y] = 1
         else:
             self.move1(randint(1,9))
 
     def move(self):
-        cells2[self.position[0]][self.position[1]]=0
-        if self.memory == () or not self.hasItem:
-            self.move1(randint(1,9))
-        else:
-            x=0
-            y=0
-            if self.memory[0] > self.position[0]:
-                x+=1
-            elif self.memory[0] < self.position[0]:
-                x-=1
-            if self.memory[1] > self.position[1]:
-                y+=1
-            elif self.memory[1] < self.position[1]:
-                y-=1
-            self.move2(self.position[0] + x,self.position[1] + y)
+        self.move1(randint(1,9))
 
-def dispose(cells, n):
+def dispose(n, alive):
     global aliveAntsList
     cont = 0
     while cont < n:
         x = randint(0,size-1)
         y = randint(0,size-1)
-        if cells[x][y] == 0:
+        if alive:
+            aliveAntsList += [AliveAnt((x,y))]
             cont+=1
-            cells[x][y] = 1
-            if cells == cells2: aliveAntsList += [AliveAnt((x,y))]
+            continue
+        if cells1[x][y] == 0:
+            cont+=1
+            cells1[x][y] = 1
+            pygame.draw.circle(win, (255,0,0), (x*10, y*10), 2)
+            pygame.display.update()
 
 
-display()
-pprint(cells1)
+t = threading.Thread(target=display)
+t2 = threading.Thread(target=screen)
+t.start()
+t2.start()
+
+for i in range(size):
+    for j in range(size):
+        if(cells1[i][j] == 0):
+            print(" ", end="")
+        else: print("X", end="")
+    print()
+#pprint(cells1)
